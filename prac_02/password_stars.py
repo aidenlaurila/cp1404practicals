c1da10a327144b050df6d4abe4fad2db9f5d49d9
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-09-28 19:33:42 +1000

    Refactor password program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
+"""Password Program that gets user password and prints asteriks"""
+
 MINIMUM_LENGTH = 7
-password = input("Enter password: ")
-while len(password) < MINIMUM_LENGTH:
-    print(f'Password must be at least {MINIMUM_LENGTH} characters long')
+def main():
+    """Gets user password and censors it"""
+    password = get_password()
+    display_blurred_password(password)
+
+def get_password():
+    """Asks user for password until its length is valid"""
     password = input("Enter password: ")
+    while len(password) < MINIMUM_LENGTH:
+        print(f'Password must be at least {MINIMUM_LENGTH} characters long')
+        password = input("Enter password: ")
+    return password
 
-print(len(password) * '*')
+def display_blurred_password(password):
+    """Prints blurred password"""
+    print(len(password) * '*')
 
+
+main()
