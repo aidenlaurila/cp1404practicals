de1db93bac5921a7d8566018b3695946fcd1739c
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-10-05 20:59:24 +1000

    Add question headings to randoms.py

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -5,14 +5,18 @@ Randoms program
 
 from random import randint
 
+# 1.
 # 10, 15, 15, 19
 # Smallest: 5, Largest: 20
 
+# 2.
 # 5, 7, 9, 3
 # Smallest: 3, Largest: 9
 # No because the range does not have a 4
 
+# 3.
 # 2.611398922353351, 4.343717151042842, 5.39977878963658
 # Smallest: 2.5, Largest: 5.5
 
+# 4.
 print(randint(1, 100))
