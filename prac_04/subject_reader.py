49dc7c532aca3a3411e59c7e720747496383e199
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-10-12 21:09:36 +1000

    Modified load_data function to return sorted data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,6 +13,7 @@ def main():
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    sorted_data = []
     input_file = open(filename)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +24,9 @@ def load_data(filename=FILENAME):
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        sorted_data.append(parts)
     input_file.close()
+    return sorted_data
 
 
 main()
