40f75af82544a662d0d5b209aa0c962a82b33778
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-10-19 14:05:24 +1000

    Add dictionary of colour codes (hex_colours.py)

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,5 @@
+COLOUR_CODES = {"amethyst": "#9966cc", "blackcoffee": "#3b2f2f", "blueviolet": "#8a2be2",
+                "brickred": "#cb4154", "canary": "#ffff99", "cardinal": "#c41e3a",
+                "celeste": "#b2ffff", "charcoal": "#36454f", "cobalt": "#0047ab",
+                "corn": "#fbec5d"}
+
