4e6ec22492d13153de5a6dee2cba990da430c736
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-10-19 21:01:16 +1000

    Add main and process records function (wimbledon.py)

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -4,6 +4,14 @@ Estimate: 25 minutes
 Actual:
 """
 FILENAME = "wimbledon.csv"
+COUNTRY_INDEX = 1
+CHAMPION_INDEX = 2
+
+def main():
+    records = load_records(FILENAME)
+    champions_to_wins, winning_countries = process_records(records)
+    print(champions_to_wins)
+    print(winning_countries)
 
 def load_records(filename):
     records = []
@@ -12,7 +20,22 @@ def load_records(filename):
         for line in in_file:
             section = line.strip().split(",")
             records.append(section)
+
     return records
 
-records2 = load_records(FILENAME)
-print(records2)
+def process_records(records):
+    winning_countries = set()
+    champion_to_wins = {}
+
+    for record in records:
+        winning_countries.add(record[COUNTRY_INDEX])
+
+        try:
+            champion_to_wins[record[CHAMPION_INDEX]] += 1
+
+        except KeyError:
+            champion_to_wins[record[CHAMPION_INDEX]] = 1
+
+    return champion_to_wins, winning_countries
+
+main()
