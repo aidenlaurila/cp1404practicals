de902331b00656efbdd183274c43e27642ee3e19
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-10-19 16:59:58 +1000

    Complete prac 5 word_occurences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,14 +1,18 @@
 """
 Word Occurrences
 Estimate: 15 minutes
-Actual:
+Actual: 21 minutes
 """
 
 word_occurrences = {}
 text = input("Text: ")
-for word in text.split():
-    if word in word_occurrences:
+words = text.split()
+for word in words:
+    try:
         word_occurrences[word] += 1
-    else:
+    except KeyError:
         word_occurrences[word] = 1
-print(word_occurrences)
+
+max_length = max((len(word) for word in words))
+for word in sorted(word_occurrences):
+    print(f"{word:{max_length}} : {word_occurrences[word]}")
