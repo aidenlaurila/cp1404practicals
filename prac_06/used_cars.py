e64179295bdb52607e78efde04b81fa344f0186b
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-11-02 20:15:28 +1000

    Add modifications to car and used_cars programs

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -9,8 +9,14 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car('car1', 180)
     my_car.drive(30)
+    limo = Car('limo', 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
+    limo.drive(115)
+    print(limo)
+
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
 
