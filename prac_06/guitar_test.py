1848522a53fd6facdb9e2b116f728fd7efc1d150
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-11-02 21:44:20 +1000

    Add tests in guitar_test.py

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,5 @@
+from guitar import Guitar
+
+electric_guitar = Guitar("Electric Guitar", 2002, 2000)
+print(f"{electric_guitar.name} get_age() - Expected 23. Got {electric_guitar.get_age()}")
+print(f"{electric_guitar.name} is_vintage() - Expected False. Got {electric_guitar.is_vintage()}")
