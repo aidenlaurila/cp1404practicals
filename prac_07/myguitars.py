4d3032b6a89a11be5c07fa0a55737f7ab2d48da4
aidenlaurila <aiden.laurila@my.jcu.edu.au>
2025-11-11 19:33:46 +1000

    Add add_guitar function to guitars client program

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -5,6 +5,7 @@ def main():
     """..."""
     guitars = convert_file_to_guitars(FILENAME)
     guitars.sort()
+    add_guitar(guitars)
     [print(guitar) for guitar in guitars] # Debugging
 
 def convert_file_to_guitars(filename):
@@ -20,5 +21,16 @@ def convert_file_to_guitars(filename):
         guitars.append(guitar)
     in_file.close()
     return guitars
+
+def add_guitar(guitars):
+    """Adds guitar to list of guitars until user input is empty."""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+        print("")
+        guitars.append(Guitar(name, year, cost))
+        name = input("Name: ")
+
 
 main()
